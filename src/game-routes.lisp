a5bfdb78a79619bac2d0a2aa70361b4ca1e96be3
Ilpo Lehtinen <feuer@feuerx.net>
2023-08-05 11:58:44 +0300

    load codesets only when the tables are empty

--- src/game-routes.lisp
+++ src/game-routes.lisp
@@ -75,38 +75,40 @@ WHERE user_id = $1 AND picture_id = $2" id (gethash "id" (aref asked-image 0))))
 	  
 
 (defun load-counties ()
-  (execute (:insert-rows-into 'county
-	    :columns 'code 'name
+  (when (zerop (caar (query "select count(*) from county")))
+    (execute (:insert-rows-into 'county
+	      :columns 'code 'name
 
-	    :values (mapcar (lambda (code)
-			      (list (parse-integer (gethash "code" code))
-				    (->> (coerce (gethash "classificationItemNames" code) 'list)
-				      first
-				      (gethash "name"))))
-			    (coerce 
-			     (parse (slurp-utf-8 #P"/etc/pichunter/counties.json")
-				    :allow-comments t)
-			     'list)))))
+	      :values (mapcar (lambda (code)
+				(list (parse-integer (gethash "code" code))
+				      (->> (coerce (gethash "classificationItemNames" code) 'list)
+					first
+					(gethash "name"))))
+			      (coerce 
+			       (parse (slurp-utf-8 #P"/etc/pichunter/counties.json")
+				      :allow-comments t)
+			       'list))))))
 
 ;; curl https://www2.tilastokeskus.fi/fi/luokitukset/corrmaps/export/kunta_1_20160101%23maakunta_1_20160101/ -o maakunnat_kunnat.csv
 (defun load-municipalities ()
-  (execute (:insert-rows-into 'municipality
-	    :columns 'code 'county-code
-	    :values
-	    (->> (slurp #P"/etc/pichunter/maakunnat_kunnat.csv" :external-format :cp1252)
-	      (str:split #\Newline)
-	      (drop 5)
-	      (mapcar (lambda (row)
-			(let* ((splitted (str:split ";" row))
-			       (municipality-code (first splitted))
-			       (county-code (third splitted))
-			       (data (mapcar (lambda (c)
-							      (str:replace-all "'" ""
-									       (str:replace-all "\"" "" c)))
-					     (list municipality-code county-code))))
-			  (if (second data)
-			      (mapcar #'parse-integer data)))))
-	      (remove-if-not #'identity )))))
+  (when (zerop (caar (query "SELECT count(*) FROM municipality")))
+    (execute (:insert-rows-into 'municipality
+	      :columns 'code 'county-code
+	      :values
+	      (->> (slurp #P"/etc/pichunter/maakunnat_kunnat.csv" :external-format :cp1252)
+		(str:split #\Newline)
+		(drop 5)
+		(mapcar (lambda (row)
+			  (let* ((splitted (str:split ";" row))
+				 (municipality-code (first splitted))
+				 (county-code (third splitted))
+				 (data (mapcar (lambda (c)
+						 (str:replace-all "'" ""
+								  (str:replace-all "\"" "" c)))
+					       (list municipality-code county-code))))
+			    (if (second data)
+				(mapcar #'parse-integer data)))))
+		(remove-if-not #'identity ))))))
   
   
 
